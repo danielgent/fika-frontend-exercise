9f3e3d17408c9dccd61bbc4dae82027d7b35a9c0
Daniel Gent <dan@whalar.com>
2020-10-27 21:43:35 +0100

    initial fetch POC

--- App.js
+++ App.js
@@ -3,9 +3,25 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [result, setResult] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=d432b933ecc6d5642d8d2befbc40c7ac&language=en-US&page=1"
+      );
+      const result = await response.json();
+
+      console.log(result);
+
+      setResult(result);
+    };
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{JSON.stringify(result, null, 2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
